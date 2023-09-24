5f89be38cbae5644d04b92352281b51caa3b96ad
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-09-24 20:32:44 +0800

    complete doctrine

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,15 +2,74 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\UserProfile;
+use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserProfileRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello', name: 'app_hello')]
-    public function index(): Response
+    public function index2(): Response
     {
         return new Response("Welcome To Symfony");
+    }
+
+    #[Route('/', name: 'app_index')]
+    public function index(UserProfileRepository $profiles, EntityManagerInterface $entityManager): Response
+    {
+
+
+        // We need to have the user first 
+        // $user = new User();
+        // $user->setEmail('email3@test.com');
+        // $user->setPassword('12345678');
+
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+        // $profile->setName('Erwin Pogi');
+        // $profile->setTwitterUsername('Erwin');
+
+        // $profiles->add($profile, true);
+        // $entityManager->persist($profile);
+        // $entityManager->flush();
+        // $profile = $profiles->find(1);        
+        // $entityManager->remove($profile);
+        // $entityManager->flush();
+
+        return $this->render('hello/index.html.twig', []);
+    }
+
+    #[Route('/post', name: 'app_post')]
+    public function post(EntityManagerInterface $entityManager, MicroPostRepository $posts): Response
+    {
+
+        // $post = new MicroPost(); // Owning Entity
+        // $post->setTitle('Post Title Hello');
+        // $post->setText('The quick brown fox');
+        // $post->setCreated(new DateTime());
+        // // MicroPost has comments (relationship) so MicroPost can exists without a comment
+
+        $post = $posts->find(5);
+        $comment = $post->getComments()[0];
+        $post->removeComment($comment);
+
+        // $comment = new Comment(); // Belonging Entity 
+        // $comment->setText("I need to have this skill");
+        // $comment->setPost($post);
+
+        // $post->addComment($comment);
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $this->render('hello/index.html.twig');
     }
 }
--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,66 +2,70 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Form\CommentType;
+use App\Form\MicroPostType;
+use App\Repository\CommentRepository;
+
+use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Request;
-
-use App\Form\MicroPostType;
-use App\Repository\MicroPostRepository;
-use App\Entity\MicroPost;
-use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-	public function index(MicroPostRepository $post	): Response
-	{
-	return $this->render('micro_post/index.html.twig', [
-            'posts' => $post->findAll(),
+    public function index(MicroPostRepository $post): Response
+    {
+        return $this->render('micro_post/index.html.twig', [
+            //'posts' => $post->findAll(),
+            'posts' => $post->findAllWithComments()
         ]);
     }
-    
-    #[Route('/add-micro-post', name:'app_add_micro_post')]
+
+    #[Route('/add-micro-post', name: 'app_add_micro_post')]
     public function addPost(EntityManagerInterface $entityManager): Response
     {
-       $microPost = new MicroPost();
-       $microPost->setTitle("Introduction to Muay Thai");
-       $microPost->setText("The Art of Eight Limbs");
-       $microPost->setCreated(new DateTime());
-	   $entityManager->persist($microPost);
-       $entityManager->flush();
+        $microPost = new MicroPost();
+        $microPost->setTitle("Introduction to Muay Thai");
+        $microPost->setText("The Art of Eight Limbs");
+        $microPost->setCreated(new DateTime());
+        $entityManager->persist($microPost);
+        $entityManager->flush();
 
-       return new Response('success');
+        return new Response('success');
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
-    public function showOne(MicroPost $post): Response 
+    public function showOne(MicroPost $post): Response
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
         ]);
-    } 
+    }
 
-    #[Route('micro-post-show/{post}', name:"app_micro_post_show_param")]
+    #[Route('micro-post-show/{post}', name: "app_micro_post_show_param")]
     public function showOneForParam(MicroPost $post): Response
     {
-	    dd($post);
-    } 
+        dd($post);
+    }
 
 
-    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)] 
-    public function add(Request $request, EntityManagerInterface $entityManager): Response 
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
-	    $microPost = new MicroPost();
-	    $form = $this->createForm(MicroPostType::class, $microPost);
+        $microPost = new MicroPost();
+        $form = $this->createForm(MicroPostType::class, $microPost);
 
         $form->handleRequest($request);
         // handleRequest will let the form get the data that is being sent during the request and try to match to the fields that this form defines
         // including any validation constraints and you will need to know if the form is submitted 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
             // read the data from the form
             $post->setCreated(new DateTime());
@@ -72,26 +76,25 @@ class MicroPostController extends AbstractController
             $this->addFlash('success', 'Your micro post have been successfully added');
 
             // Redirect to another page
-            return $this->redirectToRoute('app_micro_post');            
-        }  
+            return $this->redirectToRoute('app_micro_post');
+        }
 
-	    return $this->renderForm('micro_post/add.html.twig', [
-	    	'form' => $form
-	    ]);
-
+        return $this->renderForm('micro_post/add.html.twig', [
+            'form' => $form
+        ]);
     }
 
-    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')] 
-    public function edit(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response 
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response
     {
         // we let the param converter works here that is why the MicroPost was injected
 
-	   $form = $this->createForm(MicroPostType::class, $post);
+        $form = $this->createForm(MicroPostType::class, $post);
 
         $form->handleRequest($request);
         // handleRequest will let the form get the data that is being sent during the request and try to match to the fields that this form defines
         // including any validation constraints and you will need to know if the form is submitted 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
             // read the data from the form
             $entityManager->persist($post);
@@ -101,12 +104,44 @@ class MicroPostController extends AbstractController
             $this->addFlash('success', 'Your micro post have been successfully updated');
 
             // Redirect to another page
-            return $this->redirectToRoute('app_micro_post');            
-        }  
+            return $this->redirectToRoute('app_micro_post');
+        }
 
-	    return $this->renderForm('micro_post/edit.html.twig', [
-	    	'form' => $form
-	    ]);
+        return $this->renderForm('micro_post/edit.html.twig', [
+            'form' => $form,
+            'post' => $post
+        ]);
+    }
 
+    #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    public function addComment(MicroPost $post, Request $request, CommentRepository $comments, EntityManagerInterface $entityManager): Response
+    {
+        // we let the param converter works here that is why the MicroPost was injected
+
+        $form = $this->createForm(CommentType::class, new Comment());
+
+        $form->handleRequest($request);
+        // handleRequest will let the form get the data that is being sent during the request and try to match to the fields that this form defines
+        // including any validation constraints and you will need to know if the form is submitted 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);
+            // read the data from the form
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            //todo: Add a flash message 
+            $this->addFlash('success', 'Your comment have been successfully updated');
+
+            // Redirect to another page
+            return $this->redirectToRoute('app_micro_post_show', [
+                'post' => $post->getId()
+            ]);
+        }
+
+        return $this->renderForm('micro_post/comment.html.twig', [
+            'form' => $form,
+            'post' => $post
+        ]);
     }
 }
